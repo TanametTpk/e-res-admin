58d7a260802b6dfed34b4ce6299b92ad790faefa
Tui Tpk <tanamet.tpk@gmail.com>
2020-08-25 18:02:25 +0700

    / network url

--- src/App.js
+++ src/App.js
@@ -5,21 +5,11 @@ import {
   Route,
 } from "react-router-dom";
 import HomePage from './pages/Home';
-import NotFound from './pages/NotFound';
-import BusinessPage from './pages/Business';
-import ProductPage from './pages/Product';
-import ZonePage from './pages/Zone';
-import OrderPage from './pages/Order';
 
 function App() {
   return (
     <Router>
-      <Route path="/" exact component={HomePage} />
-      <Route path="/business/:businessId" exact component={BusinessPage} />
-      <Route path="/business/:businessId/products" exact component={ProductPage} />
-      <Route path="/business/:businessId/zones" exact component={ZonePage} />
-      <Route path="/business/:businessId/orders" exact component={OrderPage} />
-      <Route path="/notfound" exact component={NotFound} />
+      <Route exact component={HomePage} />
     </Router>
   );
 }
--- src/configs/network.js
+++ src/configs/network.js
@@ -1,5 +1,5 @@
 
 export default {
-    SERVER: window._env_ ? window._env_.SERVER : "http://testapp.statenext.com",
-    CLIENT: window._env_ ? window._env_.CLIENT : "http://192.168.1.103:3000",
+    SERVER: window._env_ ? window._env_.SERVER : "https://testapp.statenext.com",
+    CLIENT: window._env_ ? window._env_.CLIENT : "https://tanamettpk.github.io/emenu",
 }
