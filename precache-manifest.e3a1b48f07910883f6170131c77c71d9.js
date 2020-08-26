b7c6f09f9b1014020a275bddb07cd3456094c3b1
Tui Tpk <tanamet.tpk@gmail.com>
2020-08-26 08:31:23 +0700

    Updates

---
+++ precache-manifest.e3a1b48f07910883f6170131c77c71d9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "464c2a4b57184a8429f941bca6ab9ba5",
+    "url": "/e-res-admin/index.html"
+  },
+  {
+    "revision": "6f11ef6bd5127e0c3e87",
+    "url": "/e-res-admin/static/css/2.d0176e96.chunk.css"
+  },
+  {
+    "revision": "a2ccbf38f13c08a15a47",
+    "url": "/e-res-admin/static/css/main.8e9f8467.chunk.css"
+  },
+  {
+    "revision": "6f11ef6bd5127e0c3e87",
+    "url": "/e-res-admin/static/js/2.a936a714.chunk.js"
+  },
+  {
+    "revision": "bdef64be15b83c2683ba1e88fc906387",
+    "url": "/e-res-admin/static/js/2.a936a714.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2ccbf38f13c08a15a47",
+    "url": "/e-res-admin/static/js/main.9f132f47.chunk.js"
+  },
+  {
+    "revision": "cfd61e4bf472ff2c1e70",
+    "url": "/e-res-admin/static/js/runtime-main.2f1b0e62.js"
+  },
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "/e-res-admin/static/media/sidebar-2.310509c9.jpg"
+  }
+]);
